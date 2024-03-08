64fbfd5d4c7dc2e8c4138f7993301f95eecc7a56
구본웅 <thesmartist3@gmail.com>
2024-03-08 17:33:27 +0900

    Feat: AdventureItemLog 엔티티 생성

---
+++ backend/src/db/entity/adventure-item-log.ts
@@ -0,0 +1,25 @@
+import { Column, Entity, ManyToOne } from "typeorm";
+import { CodeColumn, CreatedAt, Id } from "../typeorm-utils";
+import { PartyMember } from "./party-member";
+import { ItemCode } from "./item-code";
+
+@Entity()
+export class AdventureItemLog {
+    @Id('사용자 ID')
+    memberId: string;
+
+    @Id('모험 ID')
+    adventureId: string;
+
+    @Id('아이템 코드')
+    itemCodeId: string;
+
+    @ManyToOne(() => PartyMember)
+    partyMember: PartyMember;
+
+    @CodeColumn(ItemCode)
+    itemCode: ItemCode;
+
+    @CreatedAt('로그 생성 시간')
+    createdAt: Date;
+}
