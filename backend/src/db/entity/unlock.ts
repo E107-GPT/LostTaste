8cd799df9b9ad9c5ead6f1f68f3065559d4b3b89
구본웅 <thesmartist3@gmail.com>
2024-03-08 18:01:06 +0900

    Feat: Unlock 엔티티 생성

---
+++ backend/src/db/entity/unlock.ts
@@ -0,0 +1,27 @@
+import { Column, Entity, JoinColumn, ManyToOne } from "typeorm";
+import { CodeColumn, CreatedAt, GeneratedId } from "../typeorm-utils";
+import { Member } from "./member";
+import { ItemCode } from "./item-code";
+
+@Entity()
+export class Unlock {
+    @GeneratedId('해금 ID', 'unlock_id')
+    id: string;
+
+    @ManyToOne(() => Member, { nullable: false })
+    @JoinColumn()
+    member: Member;
+
+    @CodeColumn(ItemCode)
+    itemCode: ItemCode;
+
+    @Column({
+        type: 'int',
+        nullable: false,
+        comment: '사용한 젤리'
+    })
+    price: number;
+
+    @CreatedAt('해금 시간')
+    createdAt: Date;
+}
