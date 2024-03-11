bc4c0e1b3bb03805c9d9c1d0575da10de44bff14
구본웅 <thesmartist3@gmail.com>
2024-03-11 12:45:49 +0900

    Fix: Board @Entity 데코레이터 추가

--- backend/src/db/entity/board.ts
+++ backend/src/db/entity/board.ts
@@ -1,7 +1,8 @@
-import { Column } from "typeorm";
+import { Column, Entity } from "typeorm";
 import { CodeColumn, CreatedAt, Id, Password } from "../typeorm-utils";
 import { CommonCode } from "./common-code";
 
+@Entity({ comment: '게시판' })
 export class Board {
     @Id('게시판 ID', 'board_id')
     id: string;
