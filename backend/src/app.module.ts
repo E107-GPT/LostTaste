1b8c32f2cbc95cab5e2c53640b346e605fbccbed
구본웅 <thesmartist3@gmail.com>
2024-03-11 15:25:47 +0900

    Refactor: mongoose 모듈 삭제

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,16 +1,15 @@
 import { CacheModule } from '@nestjs/cache-manager';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { MongooseModule } from '@nestjs/mongoose';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { redisStore } from 'cache-manager-redis-yet';
 import * as path from 'path';
+import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
-import { UserModule } from './user/user.module';
-import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
 import { CodeModule } from './code/code.module';
+import { UserModule } from './user/user.module';
 
 const configModule = ConfigModule.forRoot({
   isGlobal: true,
@@ -29,8 +28,6 @@ const typeOrmModule = TypeOrmModule.forRoot({
   logging: process.env.NODE_ENV === 'dev',
 });
 
-const mongooseModule = MongooseModule.forRoot(process.env.MONGODB_URL);
-
 const redisModule = CacheModule.registerAsync({
   useFactory: async () => ({
     store: () => redisStore({
@@ -40,7 +37,7 @@ const redisModule = CacheModule.registerAsync({
 });
 
 @Module({
-  imports: [configModule, typeOrmModule, mongooseModule, redisModule, AuthModule, UserModule, CodeModule, CodeModule],
+  imports: [configModule, typeOrmModule, redisModule, AuthModule, UserModule, CodeModule, CodeModule],
   controllers: [AppController],
   providers: [AppService],
 })
