7f31d340f61cab7ddbaeee3aed52b605a2f95b10
구본웅 <thesmartist3@gmail.com>
2024-03-12 13:35:58 +0900

    Feat: JWT 관련 설정을 .env로 분리

--- backend/src/auth/auth.guard.ts
+++ backend/src/auth/auth.guard.ts
@@ -21,7 +21,7 @@ import {
         const payload = await this.jwtService.verifyAsync(
           token,
           {
-            secret: 'jwtsecret'
+            secret: process.env.JWT_SECRET
           }
         );
         // 💡 We're assigning the payload to the request object here
