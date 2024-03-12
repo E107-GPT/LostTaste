ab286bdb186cd6da032b1ba49830391be873f600
구본웅 <thesmartist3@gmail.com>
2024-03-12 17:51:57 +0900

    Feat: 기초적인 validation

--- backend/src/auth/dto/login.dto.ts
+++ backend/src/auth/dto/login.dto.ts
@@ -1,5 +1,9 @@
+import { IsAlphanumeric, IsNotEmpty, Length } from "class-validator";
+
 export class LoginDto {
+    @IsAlphanumeric()
     username: string;
 
+    @IsNotEmpty()
     password: string;
 }
--- backend/src/user/dto/signup.dto.ts
+++ backend/src/user/dto/signup.dto.ts
@@ -1,7 +1,12 @@
+import { Length } from "class-validator";
+
 export class SignupDto {
+    @Length(4, 16)
     accountId: string;
 
+    @Length(4, 16)
     password: string;
 
+    @Length(1, 16)
     nickname: string;
 }
