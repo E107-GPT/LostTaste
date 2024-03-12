c3435babd9f0fa3411ef56ae945c3d767f397070
구본웅 <thesmartist3@gmail.com>
2024-03-12 12:17:01 +0900

    Fix: user module에 UserController 추가

--- backend/src/user/user.module.ts
+++ backend/src/user/user.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Member } from 'src/db/entity/member';
+import { UserController } from './user.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([
     Member
   ])],
+  controllers: [UserController],
   providers: [UserService],
   exports: [UserService]
 })
