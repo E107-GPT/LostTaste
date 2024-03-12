6ecca4ba4967dd574ba363ddc19244722d798133
구본웅 <thesmartist3@gmail.com>
2024-03-12 12:05:04 +0900

    Fix: 공통 코드 관련 코멘트, user.module에 Member 추가

--- backend/src/user/user.module.ts
+++ backend/src/user/user.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Member } from 'src/db/entity/member';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forFeature([
+    Member
+  ])],
   providers: [UserService],
   exports: [UserService]
 })
