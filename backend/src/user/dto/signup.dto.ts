d0d4e3864ac92b33a30aaee765d884978f529cda
구본웅 <thesmartist3@gmail.com>
2024-03-13 11:30:38 +0900

    Feat: Signup 유효성 조건 변경

--- backend/src/user/dto/signup.dto.ts
+++ backend/src/user/dto/signup.dto.ts
@@ -1,10 +1,11 @@
-import { Length } from "class-validator";
+import { IsAlphanumeric, Length, Matches } from "class-validator";
 
 export class SignupDto {
     @Length(4, 16)
+    @IsAlphanumeric()
     accountId: string;
 
-    @Length(4, 16)
+    @Matches(/^[A-Za-z0-9!@#$%^&*+=-]{8, 32}$/)
     password: string;
 
     @Length(1, 16)
