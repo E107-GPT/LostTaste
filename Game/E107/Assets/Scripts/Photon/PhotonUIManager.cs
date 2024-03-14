f0f238c22c8967f680c7b5d120dec565f7bcf577
lement3864 <lement3864@gmail.com>
2024-03-14 15:39:17 +0900

    FIX: uimanager change photonUiManager

--- Game/E107/Assets/Scripts/Photon/PhotonUIManager.cs
+++ Game/E107/Assets/Scripts/Photon/PhotonUIManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class UIManager : MonoBehaviour
+public class PhotonUIManager : MonoBehaviour
 {
     #region private constants
     string roomName = "Room Name";
