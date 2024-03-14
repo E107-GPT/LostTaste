eafcb3f2ba77d5acdc2de34211441e2b0a0de304
구본웅 <thesmartist3@gmail.com>
2024-03-14 23:12:31 +0900

    Feat: board controller 틀 생성

--- backend/src/board/board.controller.ts
+++ backend/src/board/board.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
+import { BoardService } from './board.service';
 
 @Controller('board')
-export class BoardController {}
+export class BoardController {
+  constructor(
+    private readonly boardService: BoardService
+  ) { }
+
+  @Get()
+  async get() {
+
+  }
+
+  @Get(':boardId')
+  async getBoardId() {
+
+  }
+
+  @Post()
+  async post() {
+
+  }
+}
