d9a9027253585dbb2b8634a484a22297fd706ed0
구본웅 <thesmartist3@gmail.com>
2024-03-14 12:02:28 +0900

    Feat: Nest Controller가 Map을 JSON으로 response 할 수 있도록 수정

--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -5,11 +5,11 @@ import * as bcrypt from 'bcrypt';
 import { CodeService } from 'src/code/code.service';
 import { Member } from 'src/db/entity/member';
 import { MemberEquipment } from 'src/db/entity/member-equipment';
+import { DuplicatedIdException } from 'src/exception/exception';
 import { SignupDto } from 'src/user/dto/signup.dto';
 import { Repository } from 'typeorm';
 import { UserProfileDto } from './dto/user-profile.dto';
 import { UserDto } from './dto/user.dto';
-import { DuplicatedIdException } from 'src/exception/exception';
 
 @Injectable()
 export class UserService {
