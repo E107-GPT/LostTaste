a704d559a3aed9cd368f84770b6990abb02532d1
구본웅 <thesmartist3@gmail.com>
2024-03-15 16:35:40 +0900

    Fix: board module에 code module 추가

--- backend/src/board/board.module.ts
+++ backend/src/board/board.module.ts
@@ -4,12 +4,14 @@ import { BoardService } from './board.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Board } from 'src/db/entity/board';
 import { PasswordModule } from 'src/password/password.module';
+import { CodeModule } from 'src/code/code.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([
       Board
     ]),
+    CodeModule,
     PasswordModule
   ],
   controllers: [BoardController],
