72519d6d080cb4754039b52545c2c8226ee82b57
구본웅 <thesmartist3@gmail.com>
2024-03-18 15:08:42 +0900

    Feat: user getProfile() jelly 추가

--- backend/src/user/dto/user-profile.dto.ts
+++ backend/src/user/dto/user-profile.dto.ts
@@ -3,5 +3,6 @@ import { CommonCodeType } from "src/db/entity/common-code-type";
 import { UserDto } from "./user.dto";
 
 export class UserProfileDto extends UserDto {
+    jelly: number;
     lastCustom: Map<typeof CommonCodeType.prototype.id, typeof CommonCode.prototype.id>;
 }
