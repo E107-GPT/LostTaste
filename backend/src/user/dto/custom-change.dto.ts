c235f58a79e56d58d51d0c5ab2bed0387eef1702
구본웅 <thesmartist3@gmail.com>
2024-03-18 17:56:45 +0900

    Feat: custom-change.dto.ts message 추가

--- backend/src/user/dto/custom-change.dto.ts
+++ backend/src/user/dto/custom-change.dto.ts
@@ -3,9 +3,13 @@ import { CustomType } from "src/common/enums";
 import { IsCustomCode } from "src/validation/is-common-code.validator";
 
 export class CustomChangeDto {
-    @IsEnum(CustomType)
+    @IsEnum(CustomType, {
+        message: '유효하지 않은 커스텀 코드 타입입니다!'
+    })
     customType: CustomType;
 
-    @IsCustomCode()
+    @IsCustomCode({
+        message: '커스텀 코드가 잘못되었습니다!'
+    })
     code: string;
 }
