751e02b3b4fb5a79592267ae70ffc6bfdc3aae92
구본웅 <thesmartist3@gmail.com>
2024-03-18 17:54:35 +0900

    Feat: board-delete.dto message 추가

--- backend/src/board/dto/board-delete.dto.ts
+++ backend/src/board/dto/board-delete.dto.ts
@@ -1,6 +1,8 @@
 import { IsNotEmpty } from "class-validator";
 
 export class BoardDeleteDto {
-    @IsNotEmpty()
+    @IsNotEmpty({
+        message: '비밀번호는 필수입니다!'
+    })
     password: string;
 }
