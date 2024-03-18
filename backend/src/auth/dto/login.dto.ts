fcdecdeffda776b3287aad903029e2a75579cc1b
구본웅 <thesmartist3@gmail.com>
2024-03-18 17:39:26 +0900

    Feat: login.dto message 추가

--- backend/src/auth/dto/login.dto.ts
+++ backend/src/auth/dto/login.dto.ts
@@ -1,9 +1,13 @@
-import { IsAlphanumeric, IsNotEmpty, Length } from "class-validator";
+import { IsAlphanumeric, IsNotEmpty } from "class-validator";
 
 export class LoginDto {
-    @IsAlphanumeric()
+    @IsAlphanumeric('en-US', {
+        message: 'ID는 영문자 또는 숫자여야 합니다!'
+    })
     accountId: string;
 
-    @IsNotEmpty()
+    @IsNotEmpty({
+        message: '비밀번호는 필수입니다!'
+    })
     password: string;
 }
