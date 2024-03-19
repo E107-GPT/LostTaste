28c42e4bf61236d900f98179775f9e3524697046
장수영 <suyoung998@naver.com>
2024-03-19 10:46:34 +0900

    Feat: 오류 메세지 받는 방식 변경

--- Game/E107/Assets/Scripts/Networking/HTTPRequest.cs
+++ Game/E107/Assets/Scripts/Networking/HTTPRequest.cs
@@ -9,8 +9,6 @@ public class HTTPRequest : MonoBehaviour
 {
     string url = "https://j10e107.p.ssafy.io/api/";
     //string port = "443";
-    // Start is called before the first frame update
-    string accesstoken;
 
     // GET
     // 경로만 지정
@@ -28,7 +26,7 @@ public class HTTPRequest : MonoBehaviour
                 GameObject.Find("Canvas/Login SignUp Window").GetComponent<Login>().ShowConnecting();
             }
             
-            Debug.Log(request.error);
+            //Debug.Log(request.error);
         }
         else
         {
@@ -42,8 +40,6 @@ public class HTTPRequest : MonoBehaviour
 
                 GameObject.Find("Canvas/Login SignUp Window").GetComponent<Login>().MoveScene();
             }
-            // 응답 텍스트 처리
-            Debug.Log(request.downloadHandler.text);
         }
 
     }
@@ -71,8 +67,6 @@ public class HTTPRequest : MonoBehaviour
         string jsonData = jsonDataBuilder.ToString();
 
         byte[] jsonToSend = new UTF8Encoding().GetBytes(jsonData);
-        Debug.Log(jsonData);
-        Debug.Log(url + "api/" + path);
         UnityWebRequest postRequest = new UnityWebRequest(url + path, "POST");
         postRequest.uploadHandler = (UploadHandler)new UploadHandlerRaw(jsonToSend);
         postRequest.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer();
@@ -82,9 +76,8 @@ public class HTTPRequest : MonoBehaviour
 
         if (postRequest.result != UnityWebRequest.Result.Success)
         {
-            Debug.LogError("error" + postRequest.error);
-            Debug.Log("result" + postRequest.result);
-            Debug.Log(postRequest.downloadHandler.text);
+            //Debug.LogError("error" + postRequest.error);
+            //Debug.Log("result" + postRequest.result);
             UserData data = JsonUtility.FromJson<UserData>(postRequest.downloadHandler.text);
 
             if (path.Equals("user")) // 회원가입일 경우
@@ -92,19 +85,17 @@ public class HTTPRequest : MonoBehaviour
                 // 로그인 화면으로 이동
                 Login login = GameObject.Find("Canvas/Login SignUp Window").GetComponent<Login>();
                 login.SignupFailure();
-                string warn = data.message;
-                if (warn.Length < 1) warn = "ID : 4-6 영숫자    닉네임 : 1-16\n비밀번호 : 8-32 영숫자특수문자";
-                login.ShowWarnMessage(warn);
+                string msg = "ID : 4-6 영숫자    닉네임 : 1-16\n비밀번호 : 8-32 영숫자특수문자";
+                if (data.message.Length > 0)
+                    msg = data.message[0];
+                login.ShowWarnMessage(msg);
             }
             else if (path.Equals("auth/login")) // 로그인일 경우
             {
-                Debug.Log(data.message);
-                string msg = data.message;
+                string msg = "사용자 정보를 불러올 수 없습니다.";
+                if (data.message.Length>0)
+                    msg = data.message[0];
 
-                if(msg.Equals("Not Found"))
-                {
-                    msg = "사용자 정보를 불러올 수 없습니다.";
-                }
                 Login login = GameObject.Find("Canvas/Login SignUp Window").GetComponent<Login>();
                 login.LoginFailure();
                 login.ShowWarnMessage(msg);
@@ -122,18 +113,13 @@ public class HTTPRequest : MonoBehaviour
             else if (path.Equals("auth/login")) // 로그인일 경우
             {
                 UserData data = JsonUtility.FromJson<UserData>(postRequest.downloadHandler.text);
-                accesstoken = data.accessToken;
-
-                UserInfo.GetInstance().SetToken(accesstoken);
-                //info.GetInstance().SetId(data.id);
-                //info.GetInstance().SetNickName(data.nickname);
 
                 //액세스 토큰으로 id, pw 받아오는 get 보내야함
+                UserInfo.GetInstance().SetToken(data.accessToken);
 
                 GameObject.Find("Canvas/Login SignUp Window").GetComponent<Login>().ShowConnecting();
                 GetCall("user/profile");
             }
-            Debug.Log(postRequest.downloadHandler.text);
         }
     }
 
@@ -144,7 +130,7 @@ public class HTTPRequest : MonoBehaviour
         public string id;
         public string nickname;
         public string error;
-        public string message;
+        public string[] message;
     }
 
     public void POSTCall(string path, Dictionary<string, string> postParam)
