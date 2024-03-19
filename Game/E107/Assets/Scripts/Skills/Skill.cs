c772fad56ef9cfb03960a00dcc259f79f0fdfa03
최명성 <audtjd1893@gmail.com>
2024-03-19 14:29:32 +0900

    Feat: 기본공격 스킬 추가

--- Game/E107/Assets/Scripts/Skills/Skill.cs
+++ Game/E107/Assets/Scripts/Skills/Skill.cs
@@ -4,26 +4,49 @@ using UnityEngine;
 
 public abstract class Skill : MonoBehaviour
 {
-
+    public static int _seq;
+    protected string _skillName;
     protected float _lastCastTime;
     protected float _skillCoolDownTime;
     protected int _requiredMp;
+    protected Transform _root;
+
+    public string SkillName
+    {
+        get { return _skillName; } set { _skillName = value; }
+    }
     public float SkillCoolDownTime
     {
-        get { return _skillCoolDownTime; } private set { _skillCoolDownTime = value; }
+        get { return _skillCoolDownTime; } set { _skillCoolDownTime = value; }
     }
 
-    public float RequiredMp
+    public int RequiredMp
     {
         get { return _requiredMp; }
-        private set { _skillCoolDownTime = value; }
+        set { _requiredMp = value; }
+    }
+
+    public Transform Root
+    {
+        get { return _root; }
+        set { _root = value; }
     }
 
     private void Start()
     {
-        // 스킬 쿨다운 설정
+        Root = transform.root;
         Init();
     }
+
+    // 스킬 쿨다운 설정
     protected abstract void Init();
-    protected abstract int Cast(int _attackDamage);
+    protected int Cast(int _attackDamage) {
+        if (Time.time - _lastCastTime < SkillCoolDownTime) return 0;
+        StartCoroutine(SkillCoroutine(_attackDamage));
+        return RequiredMp; 
+    }
+
+    protected abstract IEnumerator SkillCoroutine(int _attackDamage, int _attackRange);
+
+
 }
