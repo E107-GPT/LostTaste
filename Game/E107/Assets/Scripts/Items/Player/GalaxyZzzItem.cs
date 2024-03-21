0349eb0ece258d7e3b73d5ad3d650d73e9123676
구본웅 <thesmartist3@gmail.com>
2024-03-21 17:49:27 +0900

    Feat: 갤럭시 ZZZ 이름과 설명 추가

--- Game/E107/Assets/Scripts/Items/Player/GalaxyZzzItem.cs
+++ Game/E107/Assets/Scripts/Items/Player/GalaxyZzzItem.cs
@@ -7,6 +7,10 @@ public class GalaxyZzzItem : Item
     protected override void Init()
     {
         base.Init();
+
+        Name = "갤럭시 ZZZ";
+        FlavorText = "이걸로 종이접기도 할 수 있어요.";
+
         _leftSkill = gameObject.GetOrAddComponent<NormalAttackSkill>();
         _rightSkill = gameObject.GetOrAddComponent<GalaxyZzzSkill>();
     }
