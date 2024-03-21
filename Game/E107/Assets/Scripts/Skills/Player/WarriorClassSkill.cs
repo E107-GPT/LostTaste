ff02a8416ff8947de72bd7adcd0c5ef2ed36297b
최명성 <audtjd1893@gmail.com>
2024-03-21 18:01:03 +0900

    Fix: 수치 조정

--- Game/E107/Assets/Scripts/Skills/Player/WarriorClassSkill.cs
+++ Game/E107/Assets/Scripts/Skills/Player/WarriorClassSkill.cs
@@ -28,13 +28,14 @@ public class WarriorClassSkill : Skill
         {
             // 투사체와 파티클 시스템을 앞으로 움직입니다.
             Collider[] monsters = Physics.OverlapSphere(Root.position, 20.0f, LayerMask.GetMask("Monster"));
-
+            
             foreach (var monster in monsters)
             {
+                Debug.Log(monster.name);
                 var mon = monster.GetComponent<MonsterController>();
                 mon.DetectPlayer = Root;
                 Vector3 dir = (Root.transform.position - mon.transform.position).normalized;
-                mon.Agent.Move(dir * Time.deltaTime * 1.0f);
+                mon.Agent.Move(dir * Time.deltaTime * 3.0f);
             }
 
             timer += Time.deltaTime; // 타이머를 업데이트합니다.
