9640ed62b90533d8df0bc66d3f622ad2cb5524e7
HocheolHwang <inpofes7849@naver.com>
2024-03-21 14:29:41 +0900

    Feat: 로그인 후 던전 씬으로 가도록 수정

--- Game/E107/Assets/Scripts/UI/Login/SceneLoader.cs
+++ Game/E107/Assets/Scripts/UI/Login/SceneLoader.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 /// <summary>
-/// Camp Scene으로 전환하는 컴포넌트입니다.
+/// Dungeon Scene으로 전환하는 컴포넌트입니다.
 /// </summary>
 public class SceneLoader : MonoBehaviour
 {
     // Camp Scene을 로드하는 메서드
     public void LoadCampScene()
     {
-        SceneManager.LoadScene("Camp");
+        SceneManager.LoadScene("Dungeon");
     }
 }
