abef5a3af05d884a005ee64e714f470a0bd8dfd1
구본웅 <thesmartist3@gmail.com>
2024-03-22 11:25:57 +0900

    Feat: 통나무 스킬 구현

--- Game/E107/Assets/Scripts/Skills/Player/LogSkill.cs
+++ Game/E107/Assets/Scripts/Skills/Player/LogSkill.cs
@@ -1,9 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class LogSkill : Skill
 {
+    private const int DAMAGE = 200;
+    private readonly Vector3 SCALE = new Vector3(5.0f, 2.0f, 5.0f);
+    private const float BREAK_PROBABILITY = 0.5f;
+
     protected override void Init()
     {
         SkillCoolDownTime = 10.0f;
@@ -12,6 +17,42 @@ public class LogSkill : Skill
 
     protected override IEnumerator SkillCoroutine(int _attackDamage, float _attackRange)
     {
-        throw new System.NotImplementedException();
+        Root = transform.root;
+        GameObject player = transform.root.gameObject;
+        PlayerController playerController = player.GetComponent<PlayerController>();
+
+        Debug.Log("Log Attack");
+        Root.GetComponent<Animator>().CrossFade("ATTACK", 0.1f, -1, 0, 0.7f);
+        yield return new WaitForSeconds(0.5f);
+
+        ParticleSystem ps = Managers.Effect.Play(Define.Effect.StrongSwingEffect, Root);
+        Transform skillObj = Managers.Resource.Instantiate("Skills/SkillObject").transform;
+        skillObj.GetComponent<SkillObject>().SetUp(Root, DAMAGE, _seq);
+
+        Vector3 offset = Root.forward.normalized * 1.5f;
+        ps.transform.position += offset;
+        skillObj.position += offset;
+
+        skillObj.localScale = SCALE;
+        skillObj.position = Root.transform.TransformPoint(Vector3.forward * (_attackRange / 2));
+        skillObj.position = new Vector3(skillObj.position.x, Root.position.y + 0.5f, skillObj.position.z);
+        skillObj.rotation = Root.rotation;
+
+        yield return new WaitForSeconds(0.1f);
+
+        if (UnityEngine.Random.Range(0.0f, 1.0f) < BREAK_PROBABILITY)
+        {
+            // ºÎ¼­Áü!
+            Debug.Log("ºÎ¼­Áü!");
+            Managers.Sound.Play("wood_break", Define.Sound.Effect, 1);
+
+            Destroy(this.gameObject);
+            playerController.ObtainWeapon("0000_Fist");
+            playerController.StateMachine.ChangeState(new IdleState(playerController));
+        }
+
+        yield return new WaitForSeconds(0.2f);
+        Managers.Resource.Destroy(skillObj.gameObject);
+        Managers.Effect.Stop(ps);
     }
 }
