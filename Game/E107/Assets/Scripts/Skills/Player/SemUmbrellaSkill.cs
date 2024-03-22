1b529f2c1e1122f8c210496b46dd26d8324ef642
구본웅 <thesmartist3@gmail.com>
2024-03-22 14:59:07 +0900

    Feat: 삼성 전기 우산 완성

--- Game/E107/Assets/Scripts/Skills/Player/SemUmbrellaSkill.cs
+++ Game/E107/Assets/Scripts/Skills/Player/SemUmbrellaSkill.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class SemUmbrellaSkill : Skill
 {
-    private const int DAMAGE = 150;
-    private readonly Vector3 SCALE = new Vector3(5.0f, 2.0f, 5.0f);
+    private const int DAMAGE = 300;
+    private readonly Vector3 SCALE = new Vector3(5.0f, 5.0f, 5.0f);
 
     protected override void Init()
     {
@@ -15,24 +15,51 @@ public class SemUmbrellaSkill : Skill
 
     protected override IEnumerator SkillCoroutine(int _attackDamage, float _attackRange)
     {
+        Root = transform.root;
         GameObject player = transform.root.gameObject;
         PlayerController playerController = player.GetComponent<PlayerController>();
+        SemUmbrellaItem item = gameObject.GetComponent<SemUmbrellaItem>();
 
         Debug.Log("SEM Umbrella Attack");
-        yield return new WaitForSeconds(0.5f);
-
         playerController.StateMachine.ChangeState(new IdleState(playerController));
+        
+        item.IsOpen = true;
+        Managers.Sound.Play("umbrella_open");
+
+        yield return new WaitForSeconds(1.0f);
 
         ParticleSystem ps = Managers.Effect.Play(Define.Effect.SemUmbrellaSkillEffect, player.transform);
         Transform skillObj = Managers.Resource.Instantiate("Skills/SkillObject").transform;
         skillObj.GetComponent<SkillObject>().SetUp(Root, DAMAGE, _seq);
 
-        skillObj.position = Root.transform.TransformPoint(Vector3.forward * (_attackRange / 2));
-        skillObj.position = new Vector3(skillObj.position.x, Root.position.y + 0.5f, skillObj.position.z);
-        skillObj.rotation = Root.rotation;
+        skillObj.localScale = SCALE;
+        skillObj.position = Root.position;
 
-        yield return new WaitForSeconds(4.0f);
+        Washout(player, Color.black);
+
+        yield return new WaitForSeconds(0.2f);
+
+        Washout(player, Color.white);
+
+        yield return new WaitForSeconds(0.8f);
+
+        item.IsOpen = false;
+
+        yield return new WaitForSeconds(3.0f);
         Managers.Resource.Destroy(skillObj.gameObject);
         Managers.Effect.Stop(ps);
+    }
+
+    public void Washout(GameObject gameObject, Color color)
+    {
+        Renderer renderer = gameObject.GetComponent<Renderer>();
+        if (renderer != null)
+            renderer.material.color = color;
+
+        for (int i=0; i < gameObject.transform.childCount; i++)
+        {
+            GameObject childObject = gameObject.transform.GetChild(i).gameObject;
+            Washout(childObject, color);
+        }
     }
 }
