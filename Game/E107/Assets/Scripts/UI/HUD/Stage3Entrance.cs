6f317f7025dbeee7120040a8d5d7bcd394f17dd3
HocheolHwang <inpofes7849@naver.com>
2024-03-25 15:29:10 +0900

    Feat: 모험 종료 시 결과 창에 시간 및 클리어한 스테이지 추가

--- Game/E107/Assets/Scripts/UI/HUD/Stage3Entrance.cs
+++ Game/E107/Assets/Scripts/UI/HUD/Stage3Entrance.cs
@@ -13,12 +13,17 @@ public class Stage3Entrance : MonoBehaviour
     [Header("[ 지도 패널 ]")]
     public TextMeshProUGUI stageText; // 스테이지 이름 텍스트
 
+    // 클리어 한 스테이지
+    [Header("[ 클리어 한 스테이지 ]")]
+    public GameObject stage2Icon; // Stage 2 클리어 아이콘
+
     // 플레이어가 캠프에 진입할 때 호출되는 메서드
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             stageText.text = "Stage 3 - 서리빛 궁전"; // 스테이지 텍스트를 캠프에 맞게 업데이트
+            stage2Icon.SetActive(true); // Stage 2 클리어 아이콘 활성화
         }
     }
 }
