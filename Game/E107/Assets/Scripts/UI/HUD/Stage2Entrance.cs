27f1214c187dca69064814ec9b053ecacd1f7855
HocheolHwang <inpofes7849@naver.com>
2024-03-26 17:11:36 +0900

    Feat: 캠프 및 던전 입장 시 입장 알림 추가

--- Game/E107/Assets/Scripts/UI/HUD/Stage2Entrance.cs
+++ Game/E107/Assets/Scripts/UI/HUD/Stage2Entrance.cs
@@ -13,6 +13,12 @@ public class Stage2Entrance : MonoBehaviour
     [Header("[ 지도 패널 ]")]
     public TextMeshProUGUI stageText; // 스테이지 이름 텍스트
 
+    // 스테이지 패널
+    [Header("[ 스테이지 패널 ]")]
+    public GameObject stagePanel; // 스테이지 패널
+    public TextMeshProUGUI stageLevelText; // 스테이지 레벨 텍스트
+    public TextMeshProUGUI stageNameText; // 스테이지 이름 텍스트
+
     // 클리어 한 스테이지
     [Header("[ 클리어 한 스테이지 ]")]
     public GameObject stageXIcon; // 클리어 한 스테이지 없음 아이콘
@@ -24,10 +30,24 @@ public class Stage2Entrance : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
-            stageText.text = "Stage 2 - 잊혀진 해변"; // 스테이지 텍스트를 캠프에 맞게 업데이트
+            stageText.text = "STAGE 2 - 잊혀진 해변"; // 스테이지 텍스트 업데이트
+
+            stageLevelText.text = "STAGE 2"; // 스테이지 레벨 텍스트를 업데이트
+            stageNameText.text = "잊혀진 해변"; // 스테이지 이름 텍스트를 업데이트
+
             stageXIcon.SetActive(false); // 클리어 한 스테이지 없음 아이콘 비활성화
             stage1Icon.SetActive(true); // Stage 1 클리어 아이콘 활성화
             stageClearText.text = "클리어한 스테이지입니다.";
+
+            StartCoroutine(ShowStagePanel());
         }
+    }
+
+    // 5초간 스테이지 패널을 활성화하고, 다시 비활성화 하는 코루틴
+    IEnumerator ShowStagePanel()
+    {
+        stagePanel.SetActive(true);
+        yield return new WaitForSeconds(5);
+        stagePanel.SetActive(false);
     }
 }
