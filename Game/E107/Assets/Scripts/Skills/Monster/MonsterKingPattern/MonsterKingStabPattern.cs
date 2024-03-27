96596e71ab38e8004eb7e4d96a8e70b74d0deeeb
김동현 <smdong97@gmail.com>
2024-03-27 09:53:59 +0900

    Feat: MonsterKing Pattern3 구현

--- Game/E107/Assets/Scripts/Skills/Monster/MonsterKingPattern/MonsterKingStabPattern.cs
+++ Game/E107/Assets/Scripts/Skills/Monster/MonsterKingPattern/MonsterKingStabPattern.cs
@@ -34,25 +34,30 @@ public class MonsterKingStabPattern : Pattern
         yield return new WaitForSeconds(0.1f);
 
         // hit box 생성
-        _leftArm = _controller.LeftArm.transform;
+        //_leftArm = _controller.LeftArm.transform;
         _stabLoc = Managers.Resource.Instantiate("Patterns/StabCollider").transform;
         _stabLoc.GetComponent<PatternObject>().Init(Root, attackDamage, _seq);
 
-        // 왼팔에 hit box와 이펙트 부착
-        _stabLoc.parent = _leftArm;
+        // 왼팔 위치에 어느 방향으로 위치하는지 확인
+        Vector3 rootLeft = Root.TransformDirection(Vector3.left);              // root의 local 좌표계를 기준으로 왼쪽으로 이동
+        Vector3 rootUp = Root.TransformDirection(Vector3.up);                  // root의 local 좌표계를 기준으로 위쪽으로 이동
+        Vector3 tempPos = Root.position + (rootUp) + (rootLeft * 0.7f);        // root의 위치에서 추가로 위쪽으로 한칸, 왼쪽으로 한칸반 이동
+        _stabLoc.position = tempPos;
         _stabLoc.rotation = Root.rotation;
+
         _particle = Managers.Effect.Play(Define.Effect.KingStabEffect, _stabLoc);      // stabLoc이랑 함께 이동
 
 
         // 이펙트와 hit box를 찌르는 애니메이션과 맞춰서 이동
-        //float speed = 50.0f;
-        //while (true)
-        //{
-        //    _stabLoc.Translate(Root.forward * speed * Time.deltaTime, Space.Self);
-        //    _particle.transform.Translate(Root.forward * speed * Time.deltaTime, Space.Self);
+        float speed = 43.0f;
+        while (true)
+        {
+            Vector3 moveStep = _stabLoc.forward * speed * Time.deltaTime;
+            _stabLoc.position += moveStep;
+            _particle.transform.position = _stabLoc.position;
 
-        //    yield return null;
-        //}
+            yield return null;
+        }
 
     }
 
