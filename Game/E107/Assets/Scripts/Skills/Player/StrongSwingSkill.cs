0c574d39432c4eab035491d10aa44e9693b131d8
구본웅 <thesmartist3@gmail.com>
2024-03-31 04:20:31 +0900

    Fix: HeroSwordSkill -> StrongSwingSkill, 통나무 적용

--- Game/E107/Assets/Scripts/Skills/Player/StrongSwingSkill.cs
+++ Game/E107/Assets/Scripts/Skills/Player/StrongSwingSkill.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HeroSwordSkill : Skill
+public class StrongSwingSkill : Skill
 {
     [field: SerializeField]
     public int Damage { get; set; }
