2b8a50ea5c51d6d4bde3d9e74d1f011b73c12e0c
HocheolHwang <inpofes7849@naver.com>
2024-04-01 03:55:21 +0900

    Feat: 아이템 기본 공격 UI 마우스 hover 시 정보 뜨는 기능 추가

--- Game/E107/Assets/Scripts/UI/Interaction/Item Attack Info Open.cs
+++ Game/E107/Assets/Scripts/UI/Interaction/Item Attack Info Open.cs
@@ -5,33 +5,33 @@ using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
 /// <summary>
-/// 대쉬 스킬 위에 마우스를 올려서 스킬 정보 UI가 나오도록 하는 클래스입니다.
+/// 아이템 공격 위에 마우스를 올려서 공격 정보 UI가 나오도록 하는 클래스입니다.
 /// </summary>
-public class DashSkillInfoOpen : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+public class ItemAttackInfoOpen : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     // ------------------------------------------------ 변수 선언 ------------------------------------------------
 
-    // 대쉬스킬 UI
-    [Header("[ 대쉬스킬 UI ]")]
-    public GameObject dashSkillInfoUI;
+    // 아이템 공격 UI
+    [Header("[ 아이템 공격 UI ]")]
+    public GameObject itemAttackInfoUI;
 
     // ------------------------------------------------ Life Cycle ------------------------------------------------
 
     // 마우스가 UI 요소 위에 올라왔을 때 호출될 메서드
     public void OnPointerEnter(PointerEventData eventData)
     {
-        if (dashSkillInfoUI != null)
+        if (itemAttackInfoUI != null)
         {
-            dashSkillInfoUI.SetActive(true); // 게임 오브젝트 활성화
+            itemAttackInfoUI.SetActive(true); // 게임 오브젝트 활성화
         }
     }
 
     // 마우스가 UI 요소에서 벗어났을 때 호출될 메서드
     public void OnPointerExit(PointerEventData eventData)
     {
-        if (dashSkillInfoUI != null)
+        if (itemAttackInfoUI != null)
         {
-            dashSkillInfoUI.SetActive(false); // 게임 오브젝트 비활성화
+            itemAttackInfoUI.SetActive(false); // 게임 오브젝트 비활성화
         }
     }
 }
