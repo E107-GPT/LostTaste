1aa60dae28bdb630f52aeebabbe56dbc8a645189
HocheolHwang <inpofes7849@naver.com>
2024-04-02 11:57:27 +0900

    Feat: 인벤토리 UI에 데미지 및 회복량 표기 되는 기능 추가

--- Game/E107/Assets/Scripts/UI/Item UI/Inventory Manager.cs
+++ Game/E107/Assets/Scripts/UI/Item UI/Inventory Manager.cs
@@ -23,16 +23,28 @@ public class InventoryManager : MonoBehaviour
     public Image firstItemIcon; // 아이템 1 아이콘
     public GameObject firstItemUsed; // 아이템 1 사용 여부
     public TextMeshProUGUI firstItemName; // 아이템 1 이름
-    public TextMeshProUGUI firstItemRightSkillMana; // 아이템 1 오른쪽 스킬 마나
-    public TextMeshProUGUI firstItemRightSkillCoolDown; // 아이템 1 오른쪽 스킬 쿨타임
+    public GameObject firstItemDamagePanel; // 아이템 1 데미지 패널
+    public GameObject firstItemHpRecoveryPanel; // 아이템 1 회복 패널
+    public GameObject firstItemMpRecoveryPanel; // 아이템 1 회복 패널
+    public TextMeshProUGUI firstItemRightSkillDamage; // 아이템 1 데미지 텍스트
+    public TextMeshProUGUI firstItemRightSkillHpRecovery; // 아이템 1 Hp 회복 텍스트
+    public TextMeshProUGUI firstItemRightSkillMpRecovery; // 아이템 1 Mp 회복 텍스트
+    public TextMeshProUGUI firstItemRightSkillMana; // 아이템 1 마나
+    public TextMeshProUGUI firstItemRightSkillCoolDown; // 아이템 1 쿨타임
 
     // 아이템 2
     [Header("[ 아이템 2 ]")]
     public Image secondItemIcon; // 아이템 2 아이콘
     public GameObject secondItemUsed; // 아이템 2 사용 여부
     public TextMeshProUGUI secondItemName; // 아이템 2 이름
-    public TextMeshProUGUI secondItemRightSkillMana; // 아이템 2 오른쪽 스킬 마나
-    public TextMeshProUGUI secondItemRightSkillCoolDown; // 아이템 2 오른쪽 스킬 쿨타임
+    public GameObject secondItemDamagePanel; // 아이템 2 데미지 패널
+    public GameObject secondItemHpRecoveryPanel; // 아이템 2 회복 패널
+    public GameObject secondItemMpRecoveryPanel; // 아이템 2 회복 패널
+    public TextMeshProUGUI secondItemRightSkillDamage; // 아이템 2 데미지 텍스트
+    public TextMeshProUGUI secondItemRightSkillHpRecovery; // 아이템 2 Hp 회복 텍스트
+    public TextMeshProUGUI secondItemRightSkillMpRecovery; // 아이템 2 Mp 회복 텍스트
+    public TextMeshProUGUI secondItemRightSkillMana; // 아이템 2 마나
+    public TextMeshProUGUI secondItemRightSkillCoolDown; // 아이템 2 쿨타임
 
 
     // ------------------------------------------------ Life Cycle ------------------------------------------------
@@ -64,6 +76,26 @@ public class InventoryManager : MonoBehaviour
         // 아이템 정보 업데이트
         UpdateItemUI(firstItemIcon, firstItemName, firstItemRightSkillMana, firstItemRightSkillCoolDown, _playerInventory[1]);
         UpdateItemUI(secondItemIcon, secondItemName, secondItemRightSkillMana, secondItemRightSkillCoolDown, _playerInventory[2]);
+
+        // 아이템 데미지 및 회복 패널 업데이트
+        UpdateItemPanelUI(
+            firstItemDamagePanel,
+            firstItemHpRecoveryPanel,
+            firstItemMpRecoveryPanel,
+            firstItemRightSkillDamage,
+            firstItemRightSkillHpRecovery,
+            firstItemRightSkillMpRecovery,
+            _playerInventory[1]
+            );
+        UpdateItemPanelUI(
+            secondItemDamagePanel,
+            secondItemHpRecoveryPanel,
+            secondItemMpRecoveryPanel,
+            secondItemRightSkillDamage,
+            secondItemRightSkillHpRecovery,
+            secondItemRightSkillMpRecovery,
+            _playerInventory[2]
+            );
     }
 
     // 아이템 UI 업데이트 메서드
@@ -83,6 +115,104 @@ public class InventoryManager : MonoBehaviour
         {
             skillMana.text = item.RightSkill.RequiredMp.ToString();
             skillCoolDown.text = $"{item.RightSkill.SkillCoolDownTime}s";
+        }
+    }
+
+    // 아이템 데미지 및 회복 UI 업데이트 메서드
+    void UpdateItemPanelUI(
+        GameObject DamagePanel,
+        GameObject HpRecoveryPanel,
+        GameObject MpRecoveryPanel,
+        TextMeshProUGUI Damage,
+        TextMeshProUGUI HpRecovery,
+        TextMeshProUGUI MpRecovery,
+        Item item)
+    {
+        // 아이템 스킬 데미지/회복 패널 및 텍스트 업데이트
+        if (item.RightSkill is ConsumingSkill)
+        {
+            // 데미지 패널 비활성화
+            DamagePanel.SetActive(false);
+
+            // Debug.Log($"회복 아이템임 {item.RightSkill}");
+
+            if (item.RightSkill is BibimbapSkill)
+            {
+                // Hp 회복 패널 활성화
+                HpRecoveryPanel.SetActive(true);
+                MpRecoveryPanel.SetActive(false);
+
+                // 회복량 업데이트
+                BibimbapSkill bibimbapSkill = (BibimbapSkill)item.RightSkill;
+                int hpRecoveryAmount = bibimbapSkill.HpRecoveryAmount;
+                HpRecovery.text = hpRecoveryAmount.ToString();
+            }
+            else if (item.RightSkill is RareSteakSkill)
+            {
+                // Hp 회복 패널 활성화
+                HpRecoveryPanel.SetActive(true);
+                MpRecoveryPanel.SetActive(false);
+
+                // 회복량 업데이트
+                RareSteakSkill rareSteakSkill = (RareSteakSkill)item.RightSkill;
+                int hpRecoveryAmount = rareSteakSkill.HpRecoveryAmount;
+                HpRecovery.text = hpRecoveryAmount.ToString();
+            }
+            else if (item.RightSkill is BoredAppleSkill)
+            {
+                // Mp 회복 패널 활성화
+                HpRecoveryPanel.SetActive(false);
+                MpRecoveryPanel.SetActive(true);
+
+                // 회복량 업데이트
+                BoredAppleSkill boredAppleSkill = (BoredAppleSkill)item.RightSkill;
+                float recoveryPeriod = boredAppleSkill.RecoveryPeriod;
+                int mpRecoveryAmountPerPeriod = boredAppleSkill.MpRecoveryAmountPerPeriod;
+                MpRecovery.text = $"{mpRecoveryAmountPerPeriod / recoveryPeriod}Mp/s";
+            }
+            else if (item.RightSkill is CucumberSkill)
+            {
+                // Mp 회복 패널 활성화
+                HpRecoveryPanel.SetActive(false);
+                MpRecoveryPanel.SetActive(true);
+
+                // 회복량 업데이트
+                CucumberSkill cucumberSkill = (CucumberSkill)item.RightSkill;
+                int mpRecoveryAmount = cucumberSkill.MpRecoveryAmount;
+                MpRecovery.text = mpRecoveryAmount.ToString();
+            }
+        }
+        else if (item.RightSkill is HealWandSkill)
+        {
+            // Hp 회복 패널 활성화
+            DamagePanel.SetActive(false);
+            HpRecoveryPanel.SetActive(true);
+            MpRecoveryPanel.SetActive(false);
+
+            // 회복량 업데이트
+            HealWandSkill healWandSkill = (HealWandSkill)item.RightSkill;
+            int hpRecoveryAmount = healWandSkill.HpRecoveryAmount;
+            HpRecovery.text = hpRecoveryAmount.ToString();
+        }
+        else if (item.RightSkill is IAttackSkill)
+        {
+            // 데미지 패널 활성화
+            DamagePanel.SetActive(true);
+            HpRecoveryPanel.SetActive(false);
+            MpRecoveryPanel.SetActive(false);
+
+            IAttackSkill attackSkill = (IAttackSkill)item.RightSkill;
+            int damage = attackSkill.Damage;
+            Damage.text = damage.ToString();
+        }
+        else if (item.RightSkill is EmptySkill)
+        {
+            DamagePanel.SetActive(true);
+            HpRecoveryPanel.SetActive(false);
+            MpRecoveryPanel.SetActive(false);
+            Damage.text = "-";
+            HpRecovery.text = "-";
+            MpRecovery.text = "-";
         }
     }
 
