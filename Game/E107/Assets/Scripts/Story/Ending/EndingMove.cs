0d5b5782eca82fd0e1b8408ab0c2576f529a02c2
신현기 <muffin3864@naver.com>
2024-04-03 01:08:06 +0900

    FEAT: ending Scene cut edit

--- Game/E107/Assets/Scripts/Story/Ending/EndingMove.cs
+++ Game/E107/Assets/Scripts/Story/Ending/EndingMove.cs
@@ -8,70 +8,107 @@ public class EndingMove : MonoBehaviour
     // 카메라 위치 배열
     private Vector3[] positions = new Vector3[]
     {
-        new Vector3(2.92f, 1.04f, 5.52f), // 용사의 마지막 공격
-
-        
+        new Vector3(271.5f, 21.2f, -479.4f), // 1.쓰러진 용사들
+        new Vector3(166.74f, 14.08f, -312.35f), // 2.쓰러진 용사들과 용사와 마왕의 대치
+        new Vector3(30f, 6.2f, -169.3f), // 3.용사와 마왕의 대치
+        new Vector3(2.92f, 1.04f, 5.52f), // 4.용사의 마지막 공격
+        new Vector3(196f, 28f, 332.1f), // 5. 마왕의 저주가 풀리는 장면
+        new Vector3(382.67f, 85.16f, 651.37f), // 6.마을에서 환영받는 용사들
+        //new Vector3(498.78f, 77.1f, 835.79f), // 7.뒷풀이 하는 용사들
     };
 
     // 카메라 회전 배열
     private Quaternion[] rotations = new Quaternion[]
     {
-        Quaternion.Euler(-8.429f, -121.765f, 3.607f),
-        
+        Quaternion.Euler(90f, -117.983f, 0f),   // 1.쓰러진 용사들
+        Quaternion.Euler(26.461f, -117.983f, 0f),   // 2.쓰러진 용사들과 용사와 마왕의 대치
+        Quaternion.Euler(0f, -120.669f, 0f),    // 3.용사와 마왕의 대치
+        Quaternion.Euler(-8.429f, -121.765f, 3.607f),   // 4.용사의 마지막 공격
+        Quaternion.Euler(-31.128f, -61.675f, 0f),   // 5. 마왕의 저주가 풀리는 장면
+        Quaternion.Euler(17.48f, -33.526f, 1.233f),   // 6.마을에서 환영받는 용사들
+        //Quaternion.Euler(22.767f, 17.795f, 1.336f),   // 7.뒷풀이 하는 용사들
     };
 
+    // 장면에서 대기할 시간
+    private float[] waitTimes = new float[]
+    {
+        3f, // 1.쓰러진 용사들
+        5f, // 2.쓰러진 용사들과 용사와 마왕의 대치
+        5f, // 3.용사와 마왕의 대치
+        3f, // 4.용사의 마지막 공격
+        5f, // 5. 마왕의 저주가 풀리는 장면
+        11f, // 6.마을에서 환영받는 용사들
+        //3f, // 7.뒷풀이 하는 용사들
+    };
+
+    private string[] storyContents = new string[]
+    {
+        "", // 1.쓰러진 용사들
+        "끝을 내자, 용사여.",  // 2.쓰러진 용사들과 용사와 마왕의 대치
+        "우리가 세계에 맛을 되찾겠다! ",    // 3.용사와 마왕의 대치
+        "여기서 끝이다, 마왕!!!",   // 4.용사의 마지막 공격
+        "", // 5. 마왕의 저주가 풀리는 장면
+        "용사들의 여정 끝에 맛의 기쁨이 되살아났고 / 세상은 다시 삶의 풍미를 느낄 수 있게 되었습니다.",    // 6.마을에서 환영받는 용사들
+        //"용사들의 용기 덕분에 / 세상은 다시금 평화를 되찾았습니다." //7.뒷풀이 하는 용사들
+    };
     private int currentPositionIndex = 0; // 현재 카메라 위치 인덱스
 
     public EndingSceneManager EndingSceneManager;
 
     public TextMeshProUGUI storyText;
 
-    private string[] storyContents = new string[]
-    {
-        "끝이다, 마왕!!!",
-        
-    };
+    public GameObject[] effects;
+
+    public AudioClip[] backgroundMusics; 
+    private AudioSource audioSource; 
+
 
     private void Start()
     {
-        MoveCameraToPosition(currentPositionIndex);
+        audioSource = GetComponent<AudioSource>(); // AudioSource 컴포넌트 가져오기
+        if (audioSource == null)
+        {
+            // AudioSource 컴포넌트가 없다면 추가
+            audioSource = gameObject.AddComponent<AudioSource>();
+        }
 
         StartCoroutine(MoveCameraPeriodically());
     }
 
     IEnumerator MoveCameraPeriodically()
     {
-        while (currentPositionIndex < positions.Length - 1)
+        for (int i = 0; i < positions.Length; i++)
         {
-            // 일정 시간 대기
-            yield return new WaitForSeconds(6f);
-
-            // 현재 위치에서 다음 위치로 인덱스 업데이트
-            currentPositionIndex++;
-
-            // 카메라 위치 이동
-            if (currentPositionIndex < positions.Length)
+            if (i >= 1)
             {
-                MoveCameraToPosition(currentPositionIndex);
+                effects[i - 1].SetActive(false);
             }
-            if (currentPositionIndex == positions.Length - 1)
+
+            MoveCameraToPosition(i);
+
+            if (i < effects.Length)
             {
-                yield return new WaitForSeconds(6f);
-                EndingSceneManager.CompleteStory(); // 스토리 완료 처리
+                effects[i].SetActive(true); // 해당 장면의 이펙트 활성화
             }
+
+            if (i < backgroundMusics.Length && backgroundMusics[i] != null)
+            {
+                audioSource.clip = backgroundMusics[i];
+                audioSource.Play();
+            }
+
+            yield return new WaitForSeconds(waitTimes[i]);
         }
+        EndingSceneManager.CompleteStory(); // 모든 스토리 장면이 끝나면 호출
     }
 
     private void MoveCameraToPosition(int positionIndex)
     {
         // 지정된 인덱스의 위치로 카메라 이동
-        if (positionIndex >= 0 && positionIndex < positions.Length)
-        {
-            transform.position = positions[positionIndex];
-            transform.rotation = rotations[positionIndex];
-            string processedText = storyContents[positionIndex].Replace("/", "\n");
-            storyText.text = processedText;
-        }
+        transform.position = positions[positionIndex];
+        transform.rotation = rotations[positionIndex];
+        string processedText = storyContents[positionIndex].Replace("/", "\n");
+        storyText.text = processedText;
     }
 
 }
