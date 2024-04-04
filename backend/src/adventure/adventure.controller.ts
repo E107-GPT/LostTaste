4c01fdc279b22a9be580917bf6c7af78dbc57e53
구본웅 <thesmartist3@gmail.com>
2024-04-05 08:57:00 +0900

    Feat: AdventureController

--- backend/src/adventure/adventure.controller.ts
+++ backend/src/adventure/adventure.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { AdventureService } from './adventure.service';
+import { AuthGuard } from 'src/auth/auth.guard';
+import { AuthUser } from 'src/auth/auth-util';
+import { UserDto } from 'src/user/dto/user.dto';
+import { AdventurePostDto } from './dto/adventure-post-dto';
 
 @Controller('adventure')
-export class AdventureController {}
+export class AdventureController {
+    constructor (
+        private readonly adventureService: AdventureService
+    ) {}
+
+    @Post()
+    @UseGuards(AuthGuard)
+    async post(@AuthUser() user: UserDto, @Body() dto: AdventurePostDto) {
+        return await this.adventureService.save(user, dto);
+    }
+}
--- backend/src/adventure/adventure.service.ts
+++ backend/src/adventure/adventure.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { UserDto } from 'src/user/dto/user.dto';
+import { AdventurePostDto } from './dto/adventure-post-dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { GameClearLog } from 'src/db/entity/game-clear-log';
 
 @Injectable()
-export class AdventureService {}
+export class AdventureService {
+    constructor (
+        @InjectRepository(GameClearLog)
+        private readonly gameClearLogRepository: Repository<GameClearLog>
+    ) {}
+
+    async save(user: UserDto, dto: AdventurePostDto): Promise<void> {
+
+    }
+}
