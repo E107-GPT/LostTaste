5eca186201d6281347286d29602dfb21ff41fbaa
구본웅 <thesmartist3@gmail.com>
2024-04-05 09:31:42 +0900

    Feat: RankingRecordsDto

---
+++ backend/src/ranking/dto/ranking-records.dto.ts
@@ -0,0 +1,19 @@
+import { AdventureLog } from "src/db/entity/adventure-log";
+
+export class RankingRecordsDto {
+    records: RankingRecordDto[]
+
+    static fromEntities(entities: AdventureLog[]): RankingRecordsDto {
+        return {
+            records: entities.map(entity => ({
+                partyName: entity.partyName,
+                playTime: Math.floor(entity.playTime / 1000)
+            }))
+        };
+    }
+}
+
+class RankingRecordDto {
+    partyName: string;
+    playTime: number;   // 초 단위
+}
