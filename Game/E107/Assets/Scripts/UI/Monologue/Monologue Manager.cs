5d7ffcfd4ba0c23d37268e97f7f96653e34c85a9
장수영 <suyoung998@naver.com>
2024-04-05 15:01:35 +0900

    Fix: post 보내는 위치 변경

--- Game/E107/Assets/Scripts/UI/Monologue/Monologue Manager.cs
+++ Game/E107/Assets/Scripts/UI/Monologue/Monologue Manager.cs
@@ -181,6 +181,34 @@ public class MonologueManager: MonoBehaviour
         chatBackground.SetActive(false);
 
         Invoke("CloseMonologue", 5f);
+
+        if (PhotonNetwork.IsMasterClient)
+        {
+            HTTPRequest request;
+            request = GameObject.Find("gm").GetComponent<HTTPRequest>();
+            string roomName = PhotonNetwork.CurrentRoom.Name;
+            int memberCount = PhotonNetwork.CurrentRoom.PlayerCount;
+
+            string printRoomName = roomName;
+            int lastIndex = printRoomName.LastIndexOf("`");
+            if (lastIndex != -1)
+                printRoomName = printRoomName.Substring(0, lastIndex);
+
+            float time = GameObject.Find("Portal-ForestEntrance-Spawn").GetComponent<DungeonEntrance>().GameTime;
+
+            Debug.Log(time);
+
+            // 로그인 요청 보내기
+            Dictionary<string, string> requestParam = new Dictionary<string, string>();
+            requestParam.Add("partyName", printRoomName);
+            requestParam.Add("memberCount", memberCount.ToString());
+            requestParam.Add("playTimeSec", time.ToString());
+            requestParam.Add("rngSeed", PhotonNetwork.CurrentRoom.CustomProperties["seed"].ToString());
+
+            Debug.Log(requestParam);
+            request.POSTCall("adventure", requestParam);
+
+        }
     }
 
     // 독백 UI를 비활성화하는 메서드
@@ -212,33 +240,6 @@ public class MonologueManager: MonoBehaviour
             returnToCampButton.SetActive(false);
             goToEndingButton.SetActive(true);
             adventureResultsWindow.SetActive(true);
-            if (PhotonNetwork.IsMasterClient)
-            {
-                HTTPRequest request;
-                request = GameObject.Find("gm").GetComponent<HTTPRequest>();
-                string roomName = PhotonNetwork.CurrentRoom.Name;
-                int memberCount = PhotonNetwork.CurrentRoom.PlayerCount;
-
-                string printRoomName = roomName;
-                int lastIndex = printRoomName.LastIndexOf("`");
-                if (lastIndex != -1)
-                    printRoomName = printRoomName.Substring(0, lastIndex);
-
-                float time = GameObject.Find("Portal-ForestEntrance-Spawn").GetComponent<DungeonEntrance>().GameTime;
-
-                Debug.Log(time);
-
-                // 로그인 요청 보내기
-                Dictionary<string, string> requestParam = new Dictionary<string, string>();
-                requestParam.Add("partyName", printRoomName);
-                requestParam.Add("memberCount", memberCount.ToString());
-                requestParam.Add("playTimeSec", time.ToString());
-                requestParam.Add("rngSeed", PhotonNetwork.CurrentRoom.CustomProperties["seed"].ToString());
-
-                Debug.Log(requestParam);
-                request.POSTCall("adventure", requestParam);
-
-            }
         }
     }
 }
