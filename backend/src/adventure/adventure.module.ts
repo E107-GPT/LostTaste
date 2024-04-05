2de1cafcad7d02892b942a2008fb62415838bc26
구본웅 <thesmartist3@gmail.com>
2024-04-05 09:04:46 +0900

    Fix: UserModule import

--- backend/src/adventure/adventure.module.ts
+++ backend/src/adventure/adventure.module.ts
@@ -1,14 +1,16 @@
 import { Module } from '@nestjs/common';
-import { AdventureController } from './adventure.controller';
-import { AdventureService } from './adventure.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AdventureLog } from 'src/db/entity/adventure-log';
+import { UserModule } from 'src/user/user.module';
+import { AdventureController } from './adventure.controller';
+import { AdventureService } from './adventure.service';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([
       AdventureLog
-    ])
+    ]),
+    UserModule
   ],
   controllers: [AdventureController],
   providers: [AdventureService]
