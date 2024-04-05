14adf44b29bac1540a3f240a47c9ae1a924eb8d4
구본웅 <thesmartist3@gmail.com>
2024-04-05 09:02:42 +0900

    Fix: AdventureLog import

--- backend/src/adventure/adventure.module.ts
+++ backend/src/adventure/adventure.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AdventureController } from './adventure.controller';
 import { AdventureService } from './adventure.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AdventureLog } from 'src/db/entity/adventure-log';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([
+      AdventureLog
+    ])
+  ],
   controllers: [AdventureController],
   providers: [AdventureService]
 })
